d21711bac74c900bcca0fd8f9381ef837a12fc7a
francis2016e <emechidiebere1998@gmail.com>
2025-07-11 22:05:41 +0100

    added mongoDB features

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () =>{
+try {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("mongoDb connected successfully")
+} catch (error) {
+  console.error("mongoDb connection failed: ", error.message);
+  process.exit(1)
+}
+}
+module.exports = connectDB;
